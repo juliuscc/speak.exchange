b66294268df6c9a3c19067146144f6f7858fddf5
Blenda Fröjdh <blendaf@kth.se>
2020-02-25 09:41:35 +0100

    Updates theme colors (#11)

--- utils/theme.js
+++ utils/theme.js
@@ -7,14 +7,13 @@ const theme = {
   colors: {
     white: '#fff',
     black: '#000',
-    darkGrey: '#273138',
-    lightGrey: '#ededed',
-    primary: '#F38D74',
-    primaryDark: '#B63E28',
-    secondary: '#273138',
-    background: '#F38D74',
-    selection: '#ffc4b7',
-    link: '#0000ee'
+    focusBackground: '#CCCCCC',
+    text: '#3C3C3C',
+    primary: '#05B8E6',
+    primaryMuted: '#86C6DE',
+    primaryInactive: 'rgba(5, 184, 230, 0.5)',
+    primaryMutedInactive: 'rgba(134, 198, 222, 0.5)',
+    selection: '#86C6DE'
   },
   fonts: {
     title: `'${fontNames.title}', '${fontNames.text}', sans-serif`,
