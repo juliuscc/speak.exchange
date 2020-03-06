c0ec3ec42cc28b32360838568ca8f976eb34debe
catarinallens <59504633+catarinallens@users.noreply.github.com>
2020-03-06 14:12:02 +0100

    feat: adds favicon (#56)
    * Favicon
    
    * ft favicon
    
    deleted code lines from previous different project
    
    * fix: fix prettier errors
    
    Co-authored-by: cads <cads@kth.se>
    Co-authored-by: juliuscc <julius.cc@hotmail.com>

--- presentational/Meta.js
+++ presentational/Meta.js
@@ -13,36 +13,12 @@ const Meta = () => (
       )}:300,600,700|${fontNames.title.replace(' ', '+')}:600,800`}
       rel="stylesheet"
     />
-
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/static/favicon/apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/static/favicon/favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/static/favicon/favicon-16x16.png"
-    />
-    <link rel="manifest" href="/static/favicon/site.webmanifest" />
-    <link
-      rel="mask-icon"
-      href="/static/favicon/safari-pinned-tab.svg"
-      color="#5bbad5"
-    />
-    <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
     <meta name="msapplication-TileColor" content="#da532c" />
-    <meta
-      name="msapplication-config"
-      content="/static/favicon/browserconfig.xml"
-    />
     <meta name="theme-color" content="#ffffff" />
   </Head>
 )
