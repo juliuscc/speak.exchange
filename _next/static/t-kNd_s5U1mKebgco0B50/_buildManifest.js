0a4b3a43ccace18c4574ad4d40703a4da8aeaae2
Julius Celik <julius.cc@hotmail.com>
2020-03-22 10:47:47 +0000

    Updates

--- _next/static/t-kNd_s5U1mKebgco0B50/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/edit-deck":[a],"/index":[a],"/repeat":[a]}}("static\u002Fchunks\u002F598243042130f6d676736022c01eef31ec9c7cfd.033bb56a508bf80815cd.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
